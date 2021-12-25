bc336d84ec7022b4a32206fdbb976d131dc01103
Tiyarong Gurung <71687703+tiyaronggurung@users.noreply.github.com>
2021-12-24 22:56:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+/*  1.Always Hungry  */
+function alwaysHungry(arr) {
+    var foodCount = 0;
+    for(var i=0; i<arr.length; i++) {
+        if(arr[i] == "food") {
+            console.log("yummy");
+            foodCount++;
+        }
+    }
+    if(foodCount == 0) {
+        console.log("I'm hungry")
+    }
+}
+
+/*  2. High Pass Filter  */
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    for(var i=0; i<arr.length; i++) {
+        if(arr[i] > cutoff) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
+
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result);   // it should give us  [6, 8, 10, 9]
+
+/*  3. Better than average */
+function betterThanAverage(arr) {
+    var sum = 0;
+
+    for(var i=0; i<arr.length; i++) {
+        sum += arr[i];
+    }
+
+    var avg = sum / arr.length;
+    var count = 0
+
+    for(var i=0; i<arr.length; i++) {
+        if(arr[i] > avg) {
+            count++;
+        }
+    }
+    return count;
+}
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); // it should give us 4
+
+/*  4. Array Reverse  */
+function reverse(arr) {
+    var left = 0;
+    var right = arr.length - 1;
+    while(left < right) {
+        var temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+    
+    return arr;
+}
+
+var result = reverse(["a", "b", "c", "d", "e"]);
+console.log(result);
+
+/*  5. Fibonacci Array  */
+function fibonacciArray(n) {
+    
+    var fibArr = [0, 1]; // intial array = [0, 1] 
+    while(fibArr.length < n) {
+        var prev = fibArr[fibArr.length-1];
+        var prevprev = fibArr[fibArr.length-2];
+        fibArr.push(prev + prevprev);
+    }
+    return fibArr;
+}
+
+var result = fibonacciArray(10);
+console.log(result); // we should give us [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
